451b3144fcd96b9661953fa9a6a9d23eab3714aa
Sagar Gupta <sagarguptha35@gmail.com>
2023-11-22 16:07:02 +0530

    uploading whole project

---
+++ app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
@@ -0,0 +1,29 @@
+package com.example.inventory.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+/**
+ * Database class with a singleton Instance object.
+ */
+
+
+@Database(entities = [Item::class], version = 1, exportSchema = false)
+abstract class InventoryDatabase: RoomDatabase() {
+    abstract fun itemDao(): ItemDao
+    companion object{
+        @Volatile
+        private var Instance: InventoryDatabase? = null
+        fun getDatabase(context: Context): InventoryDatabase{
+            //if the instance if it is not null else build this inside the synchronised block
+            return Instance ?: synchronized(this){
+                Room.databaseBuilder(context, InventoryDatabase::class.java, "item_database")
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/inventory/ui/item/ItemEditViewModel.kt
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.inventory.ui.item
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.inventory.data.ItemsRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+/**
+ * ViewModel to retrieve and update an item from the [ItemsRepository]'s data source.
+ */
+class ItemEditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val itemsRepository: ItemsRepository
+) : ViewModel() {
+
+    /**
+     * Holds current item ui state
+     */
+    var itemUiState by mutableStateOf(ItemUiState())
+        private set
+
+    init {
+//        viewModelScope.launch{
+//            Log.d("TAG", itemId.toString())
+//            itemsRepository.getItemStream(itemId)
+//                .collect{
+//                    itemUiState = it?.toItemUiState(true) ?: ItemUiState(
+//                        itemDetails = ItemDetails(name = "Test", price = "23", quantity = "69")
+//                    )
+//                }
+//        }
+        updateUiState()
+    }
+
+    private val itemId: Int = checkNotNull(savedStateHandle[ItemEditDestination.itemIdArg])
+
+    private fun updateUiState(){
+        viewModelScope.launch {
+            itemUiState = itemsRepository.getItemStream(ItemDetailsDestination.itemId)
+                .filterNotNull()
+                .first()
+                .toItemUiState(true)
+        }
+    }
+
+    fun updateUiState(itemDetails: ItemDetails) {
+        itemUiState =
+            ItemUiState(itemDetails = itemDetails, isEntryValid = validateInput(itemDetails))
+    }
+
+    private fun validateInput(uiState: ItemDetails = itemUiState.itemDetails): Boolean {
+        return with(uiState) {
+            name.isNotBlank() && price.isNotBlank() && quantity.isNotBlank()
+        }
+    }
+
+    suspend fun updateItem(){
+        if(validateInput(itemUiState.itemDetails)){
+            itemsRepository.updateItem(itemUiState.itemDetails.toItem())
+        }
+    }
+}
